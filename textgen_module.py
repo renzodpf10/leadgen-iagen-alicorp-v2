b403653611cd19b7b958cd0edc893066944769d3
renzodpf10 <renzodelpozo@gmail.com>
2025-07-15 06:19:05 -0500

    Add files via upload

--- textgen_module.py
+++ textgen_module.py
@@ -1,5 +1,8 @@
-
 from transformers import pipeline
 
 # Cargar pipeline de generación de texto con un modelo preentrenado (como Flan-T5)
 text_generator = pipeline("text2text-generation", model="google/flan-t5-base")
+
+def generate_product_description(prompt):
+    result = text_generator(prompt, max_length=100, do_sample=True)[0]["generated_text"]
+    return result
