5691a86bbac9192b1fc2d6d0ffa815de3c1e9ea9
renzodpf10 <renzodelpozo@gmail.com>
2025-07-15 11:38:54 -0500

    Update imagegen_module.py

--- imagegen_module.py
+++ imagegen_module.py
@@ -1,13 +1,24 @@
-
 import requests
 
 def generate_product_image(prompt, api_token):
-    api_url = "https://api-inference.huggingface.co/models/prompthero/openjourney"
-    headers = {"Authorization": f"Bearer {api_token}"}
-    payload = {"inputs": prompt}
+    url = "https://openrouter.ai/api/v1/images/generations"
+    
+    headers = {
+        "Authorization": f"Bearer {api_token}",
+        "HTTP-Referer": "https://leadgen-iagen-alicorp-v2.streamlit.app",  # ¡Este debe coincidir con tu app!
+        "Content-Type": "application/json"
+    }
 
-    response = requests.post(api_url, headers=headers, json=payload)
+    data = {
+        "model": "stabilityai/sdxl-turbo",
+        "prompt": prompt,
+        "num_images": 1,
+        "size": "512x512"
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+    
     if response.status_code == 200:
-        return response.content
+        return response.json()["data"][0]["url"]
     else:
         raise Exception(f"Error generando imagen: {response.status_code} - {response.text}")
