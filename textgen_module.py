b3558ac248431dc7a4ab89b299d5f9353bfb1145
renzodpf10 <renzodelpozo@gmail.com>
2025-07-15 07:33:19 -0500

    Delete textgen_module.py

--- textgen_module.py
+++
@@ -1,5 +0,0 @@
-
-from transformers import pipeline
-
-# Cargar pipeline de generación de texto con un modelo preentrenado (como Flan-T5)
-text_generator = pipeline("text2text-generation", model="google/flan-t5-base")
